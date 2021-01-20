7bdb5331aa79ac1eb4b73de3b87be27ad93d07eb
Nathan Keith <nashvegasnate@gmail.com>
2021-01-19 20:45:17 -0600

    worked through array methods

---
+++ main.js
@@ -0,0 +1,12 @@
+const array1 = ['cow', 'dog', 'cat'];
+const string1 = array1.join();
+const string1ToArray = string1.split(',');
+//console.log(string1);
+//console.log(string1ToArray);
+
+// .push() & .pop()
+/*array11.pop();
+console.log(array1);
+array1.push('cat');
+console.log(array1);
+*/
