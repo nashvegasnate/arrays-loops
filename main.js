3f436e641d80cdef0497330390d22f397feef640
Nathan Keith <nashvegasnate@gmail.com>
2021-01-19 19:43:26 -0600

    ran some arrays

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("CONNECTED")
+//console.log("CONNECTED")
+
+const students = ['Katy', 'Jackie', 'Sean', 'Nathan'];
+//console.log(students.length);
+
+//console.log(students[students.length - 1]);
+students[0] = 'Matthew';
+//console.log(students);
+//replaced Katy with Matthew
+
+const valuePrinter = (array, index) => {
+  return array[index];
+}
+//console.log(valuePrinter(students, 2));
+//returns Sean
+
+// console.log([1,2,3,4,5,6], 4);
+// returns (6) [1,2,3,4,5,6] 4
+
+// const classmates = ['Greg', 'Jim', 'Mandy'];
+  // console.log(classmates.includes('Greg'));
+// output: true
